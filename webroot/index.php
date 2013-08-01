d388b2796946ccca48da2073ab8d8e4d0adc55a7
sbxjoy <lx_kkudng@163.com>
2013-08-01 10:56:35 +0800

    asdf

--- webroot/index.php
+++ webroot/index.php
@@ -1,8 +1,2 @@
 <?php
-$action = @$_REQUEST['action'];
-$action = $action ? $action : 'list';
-$controller = new Controller();
-$assginVals = $controller->doAction($action.'Action');
-$view = new View($assginVals);
-$view->doView($action.'View');
-
+require_once (dirname(__FILE__).'/../framework/router.php');
