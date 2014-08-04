bbf752da60e9bfcc4335c97b2788bb85d67b6566
lvxiang <lvxiang@dz01v.opend.corp.qihoo.net>
2014-08-04 18:52:02 +0800

    清理文件

--- framework/frame_const.php
+++
@@ -1,5 +0,0 @@
-<?php
-class FrameWorkConstants
-{
-    const FILE_NOT_EXISTS = '该地址不存在!';
-}
--- webroot/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(__FILE__).'/auto_load.php');
-require_once (dirname(__FILE__).'/../framework/router.php');
